9756df93b1195493a97d873c0564c299d740ef46
kjh910 <god12nom@gmail.com>
2022-01-27 08:11:56 +0900

    fix some env

--- backend/app/runserver.sh
+++ backend/app/runserver.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-echo "Waiting for database..."
-
-while ! nc -z $DJANGO_DATABASE_HOSTNAME $DJANGO_DATABASE_PORT; do
-    echo "DB not ready, keep waiting..."
+while ! nc -z $DB_HOSTNAME $DB_PORT; do
+    echo "DB not ready, wait please"
     sleep 0.1
 done
 
-echo "database started"
+echo "db started"
 python manage.py makemigrations
 python manage.py migrate
 
 python manage.py flush --noinput -v 2
-python manage.py loaddata init/fixtures.json
+python manage.py loaddata init/init.json
 python manage.py collectstatic --noinput
-# python manage.py runserver 0.0.0.0:6001
 gunicorn --bind 0.0.0.0:6001 config.wsgi:application
